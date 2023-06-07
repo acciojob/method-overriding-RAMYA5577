584182859e5ee2b31529da3c9729dc945f3b9b4a
Ramya <mutyamramya0101@gmail.com>
2023-06-07 16:28:07 +0530

    mm

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,8 @@ package com.driver;
 public class Main {
         public static void main(String args[]){
       A a=new B();
-      B b=new B();
+//      B b=new A();
 //      System.out.println(a.meth());
-      System.out.println(b.meth());
+      System.out.println(a.meth());
     }
 }
