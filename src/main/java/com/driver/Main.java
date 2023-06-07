e8477a5bff7d5406421081a3bf42a4c5a8b43d6c
Ramya <mutyamramya0101@gmail.com>
2023-06-07 12:08:14 +0530

    rowdy

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,9 @@
 package com.driver;
 public class Main {
         public static void main(String args[]){
-      B b=new B();
+      A a=new B();
 //      A a=new A();
 //      System.out.println(a.meth());
-      System.out.println(b.meth());
+      System.out.println(a.meth());
     }
 }
