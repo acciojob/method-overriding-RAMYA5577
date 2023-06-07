2a1e26799f27f8f4f9ee4b6a64c36d22154bad0e
Ramya <mutyamramya0101@gmail.com>
2023-06-07 12:04:45 +0530

    dimpy

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,9 @@
 package com.driver;
 public class Main {
         public static void main(String args[]){
-
       B b=new B();
-      A a=new A();
-      System.out.println(a.meth());
+//      A a=new A();
+//      System.out.println(a.meth());
       System.out.println(b.meth());
     }
 }
